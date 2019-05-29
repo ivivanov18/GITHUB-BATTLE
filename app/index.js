5d60f3654b15f8c0ab4f989c17b1d9ea5b487e10
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-05-29 20:30:33 +0300

    style: add styling

--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Popular from "./components/Popular";
 import "./index.css";
 
 class App extends Component {
   render() {
-    return <div>Github Battle! Yeah!!!</div>;
+    return (
+      <div className="container">
+        <Popular />
+      </div>
+    );
   }
 }
 
