3820cb599d64b3c35159afdff2ea5d20556add6d
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-06-02 18:41:33 +0300

    feat: add caching capacity for fetched projects

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,26 +1,68 @@
 import React, { Component } from "react";
-
 import LanguagesNav from "./LanguagesNav";
+
+import { fetchPopularRepos } from "../utils/api";
 
 class Popular extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: "All",
+      error: null,
+      repos: {}
     };
     this.updateLanguage = this.updateLanguage.bind(this);
   }
 
+  componentDidMount() {
+    this.updateLanguage("All");
+  }
+
   updateLanguage(selectedLanguage) {
-    this.setState({ selectedLanguage });
+    this.setState({ selectedLanguage, error: null });
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then(data => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
+          }));
+        })
+        .catch(error => {
+          console.warn(
+            "There was an error fetching the repository. The error is:"
+          );
+          console.error({ error });
+        });
+    }
+  }
+
+  isLoading() {
+    const { selectedLanguage } = this.state;
+    return (
+      this.state.repos[selectedLanguage] === undefined &&
+      this.state.error === null
+    );
   }
 
   render() {
+    const { selectedLanguage, repos, error } = this.state;
+
     return (
-      <LanguagesNav
-        onSelectLanguage={this.updateLanguage}
-        selected={this.state.selectedLanguage}
-      />
+      <React.Fragment>
+        <LanguagesNav
+          onSelectLanguage={this.updateLanguage}
+          selected={selectedLanguage}
+        />
+        {this.isLoading() && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {repos[selectedLanguage] && (
+          <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>
+        )}
+      </React.Fragment>
     );
   }
 }
