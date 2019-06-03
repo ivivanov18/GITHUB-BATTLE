be4e6ed4ea1eac7e4da32fd6fb917757338d6f1d
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-06-03 08:57:15 +0300

    modify to start rendering Battle instead of Popular component for coding purposes

--- app/index.js
+++ app/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Popular from "./components/Popular";
+import Battle from "./components/Battle";
+// import "./normalize.css";
 import "./index.css";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Popular />
+        <Battle />
       </div>
     );
   }
